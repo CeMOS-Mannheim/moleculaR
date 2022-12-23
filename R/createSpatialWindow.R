359f630fe63c5f675aa3159eaadfd684c452b059
denis-abusammour <d.abu-sammour@hs-mannheim.de>
2022-12-23 18:05:46 +0100

    edited createSpatialWindow

--- R/createSpatialWindow.R
+++ R/createSpatialWindow.R
@@ -6,6 +6,8 @@
 #' @param pixelCoords: 	a data frame with x and y columns containing the coordinates
 #' of all pixels within a given MSI data. A `matrix` will be coerced into a
 #' data frame.
+#' @param isPolygon: a logical, whether `pixelCoords` represents the coordinates
+#' of a polygon or not.
 #' @param clean: logical, whether to fill holes created by missing pixels.
 #' @param plot:  whether to plot.
 #' @return
@@ -14,13 +16,13 @@
 #' @export
 #'
 createSpatialWindow <- function(pixelCoords,
+                                isPolygon = FALSE,
                                 clean = FALSE,
                                 plot = TRUE) {
 
 
       #// checks
       if(!is.data.frame(pixelCoords)){
-
             if(is.matrix(pixelCoords)){
                   pixelCoords <- as.data.frame(pixelCoords)
             } else {
@@ -30,8 +32,37 @@ createSpatialWindow <- function(pixelCoords,
 
       }
 
+      if(ncol(pixelCoords) != 2){
+            stop("pixelCoords must be a matrix or data.frame of two columns named 'x' and 'y'. \n")
+      }
+
       # create the spwin from the raster
-      spwin     <- spatstat.geom::as.polygonal(spatstat.geom::owin(mask = pixelCoords))
+      if(isPolygon){
+
+            if(!spatstat.utils::verify.xypolygon(pixelCoords)){
+                  stop("PixelCoords is not a polygon. \n")
+            }
+
+            if(spatstat.utils::Area.xypolygon(pixelCoords) < 0){
+                  warning("The plogon provided as pixelCoords has a negative area i.e. ",
+                          "traversed clockwise (which for spatstat represents a hole). ",
+                          "The order of the points was reversed. \n")
+
+                  pixelCoords <- spatstat.utils::reverse.xypolygon(pixelCoords)
+
+            }
+
+            spwin       <- spatstat.geom::owin(poly = pixelCoords)
+
+      } else {
+
+
+            spwin       <- spatstat.geom::as.polygonal(spatstat.geom::owin(mask = pixelCoords))
+
+      }
+
+
+
 
       # remove holes
       if(clean){
